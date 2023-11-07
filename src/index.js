91a45ff46abed3691847d51892e60fc3885022a7
Abdeel Lucena porpino <105687102+AbdeelLucena@users.noreply.github.com>
2023-11-07 13:50:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
+// importação do react
 import ReactDOM from "react-dom";
+// importação do react-dom no react
 import App from "./App";
+// importação do App no diretório App
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,3 +11,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+// nesse código está utilizando o ReactDOM.render no qual está fornecendo métodos específicos para o DOM ; dentro deletemos o ReactStrictMode, no qual é uma ferramenta para sinalizar potenciais problemas em uma aplicação
